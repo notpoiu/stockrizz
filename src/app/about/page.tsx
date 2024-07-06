cd0d26048631955271721146f2eafb71fe029c9d
Nathan Shin <mastertofu12345@gmail.com>
2024-07-05 22:17:04 -0400

    feat: about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,25 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+export default function AboutPage() {
+    return (
+        <main className="flex flex-col justify-center items-center w-screen h-screen">
+            <div className="max-w-[500px]">
+                <h1 className="text-3xl">About</h1>
+                <p className="mb-2">Stockrizz is the best rizz engine</p>
+
+                <p className="font-bold">Q: What is a rizz engine?</p>
+                <p className="mb-2">A: A rizz engine is a tool that helps you analyse your rizz from a screenshot of your messages</p>
+
+                <p className="font-bold">Q: How does Stockrizz work?</p>
+                <p className="mb-2">A: Stockrizz under the hood uses a large language model using the generative pretrained transformer architecture (GPT-4o) to analyse your rizz.</p>
+
+                <p className="font-bold">Q: How accurate is Stockrizz?</p>
+                <p className="mb-2">A: Stockrizz can make some mistakes, if you are a Developer and want to help reduce these errors you may find the full site source code <Link href="">here</Link></p>
+            </div>
+            <Link href={"/"}>
+                <Button>Go Back</Button>
+            </Link>
+        </main>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,24 @@
+import { Button } from "@/components/ui/button";
 import { UploadComponent } from "@/components/upload-component";
+import { InfoIcon } from "lucide-react";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex flex-col justify-center items-center w-screen h-screen">
-      <h1 className="text-3xl">Stockrizz</h1>
+      <div className="flex flex-row">
+        <h1 className="text-3xl">Stockrizz</h1>
+        <Link href="/about">
+          <Button variant={"outline"} className="ml-2">
+            <InfoIcon />
+          </Button>
+        </Link>
+      </div>
       <p className="mb-5">The best rizz engine</p>
+      
 
       <div className="flex flex-row">
+        
         <UploadComponent />
       </div>
     </main>
