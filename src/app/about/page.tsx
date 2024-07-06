ee08a53cdefecea0dba46cfe1738ade6ac5febb1
Nathan Shin <mastertofu12345@gmail.com>
2024-07-05 22:26:10 -0400

    fix: bomboclat

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -15,7 +15,7 @@ export default function AboutPage() {
                 <p className="mb-2">A: Stockrizz under the hood uses a large language model using the generative pretrained transformer architecture (GPT-4o) to analyse your rizz.</p>
 
                 <p className="font-bold">Q: How accurate is Stockrizz?</p>
-                <p className="mb-2">A: Stockrizz can make some mistakes, if you are a Developer and want to help reduce these errors you may find the full site source code <Link href="">here</Link></p>
+                <p className="mb-2">A: Stockrizz can make some mistakes, if you are a Developer and want to help reduce these errors you may find the full site source code <Link href="https://github.com/notpoiu/stockrizz">here</Link></p>
             </div>
             <Link href={"/"}>
                 <Button>Go Back</Button>
