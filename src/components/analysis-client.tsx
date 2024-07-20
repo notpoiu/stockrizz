cea56a511227a710a332506b08d2d1c0056b2690
upio <75510171+notpoiu@users.noreply.github.com>
2024-07-20 17:18:07 -0400

    style: put icon next to text via inline-block
    good to know 😱

--- src/components/analysis-client.tsx
+++ src/components/analysis-client.tsx
@@ -17,11 +17,11 @@ export default function AnalysisClientPage({data, id}: {data: RizzAnalysis | nul
             <h1 className="text-3xl">Analysis</h1>
             <p className="mb-2">Your estimated rizz elo: <span className="font-bold">{data.overall_rating}</span></p>
             <div className="w-screen flex flex-col justify-center items-center">
-                <div className="flex flex-wrap items-start max-w-[50vw] mb-3">
-                    <p className="flex-1 mb-0">
+                <div className="max-w-[50vw] mb-3">
+                    <span className="mb-0">
                         To get more insight press on the rating icons such as
-                    </p>
-                    <Image src={`/icons/best.svg`} alt={"best"} width={24} height={24} className="flex-shrink-0 ml-2 self-end" />
+                    </span>
+                    <Image src={`/icons/best.svg`} alt={"best"} width={24} height={24} className="inline-block ml-1" />
                 </div>
                 
                 <div className="flex flex-col px-2 py-2 w-[40vw] *:mb-4">
