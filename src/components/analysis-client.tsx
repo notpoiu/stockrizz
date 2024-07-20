5d5a32030d9ca4dc73a9b0208f152eafd1813ffe
upio <75510171+notpoiu@users.noreply.github.com>
2024-07-20 17:11:39 +0000

    fix: mobile overflow issue

--- src/components/analysis-client.tsx
+++ src/components/analysis-client.tsx
@@ -13,18 +13,21 @@ export default function AnalysisClientPage({data, id}: {data: RizzAnalysis | nul
     }
     
     return (
-        <main className="flex flex-col justify-center items-center w-screen h-screen">
+        <main className="mt-5 text-center">
             <h1 className="text-3xl">Analysis</h1>
             <p className="mb-2">Your estimated rizz elo: <span className="font-bold">{data.overall_rating}</span></p>
-            <p className="mb-3 flex-row flex max-w-[50vw]">To get more insight press on the rating icons such as <Image src={`/icons/best.svg`} alt={"best"} width={24} height={24} className="ml-2" /></p>
-            
-            <div className="flex flex-col px-2 py-2 w-[40vw] *:mb-4">
-                {data && data.analysis.map((message, index) => {
-                    return <TextBubble message={message} from={message.from} key={index} /> 
-                })}
+            <div className="w-screen flex justify-center items-center">
+                <p className="mb-3 flex-row flex max-w-[50vw]"><span className="shrink">To get more insight press on the rating icons such as</span><Image src={`/icons/best.svg`} alt={"best"} width={24} height={24} className="ml-2 shrink" /></p>
+                
+                <div className="flex flex-col px-2 py-2 w-[40vw] *:mb-4">
+                    {data && data.analysis.map((message, index) => {
+                        return <TextBubble message={message} from={message.from} key={index} /> 
+                    })}
+                </div>
+    
+                <Link href="/"><Button>Try for yourself!</Button></Link>
             </div>
 
-            <Link href="/"><Button>Try for yourself!</Button></Link>
 
             <footer className="fixed bottom-0 left-0">
                 <p className="text-xs text-muted-foreground mb-2 ml-2">ID: {id}</p>
