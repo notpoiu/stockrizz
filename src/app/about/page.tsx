dda5fd56f6cdd8e181151ffd16ac6552b5b4a615
upio <75510171+notpoiu@users.noreply.github.com>
2024-07-23 12:08:36 -0400

    refactor: less "fancy" about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -9,13 +9,13 @@ export default function AboutPage() {
                 <p className="mb-2">Stockrizz is the best rizz engine</p>
 
                 <p className="font-bold">Q: What is a rizz engine?</p>
-                <p className="mb-2">A: A rizz engine is a tool that helps you analyse your rizz from a screenshot of your messages</p>
+                <p className="mb-2">A: A rizz engine is a tool that helps you analyse your rizz from your messages</p>
 
                 <p className="font-bold">Q: How does Stockrizz work?</p>
-                <p className="mb-2">A: Stockrizz under the hood uses a large language model using the generative pretrained transformer architecture (GPT-4o) to analyse your rizz.</p>
+                <p className="mb-2">A: Stockrizz under the hood uses a openai's GPT-4o to analyse your rizz.</p>
 
                 <p className="font-bold">Q: How accurate is Stockrizz?</p>
-                <p className="mb-2">A: Stockrizz can make some mistakes, if you are a Developer and want to help reduce these errors you may find the full site source code <Link href="https://github.com/notpoiu/stockrizz">here</Link></p>
+                <p className="mb-2">A: Stockrizz can make some mistakes, if you are a Developer or a Prompt Engineer, and want to help reduce these errors you may find the full site source code @ <Link href="https://github.com/notpoiu/stockrizz">Github</Link></p>
             </div>
             <Link href={"/"}>
                 <Button>Go Back</Button>
